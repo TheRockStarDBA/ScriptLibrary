58f702f1d9e7febc17529d301f055a0b496cce51
Aaron Morelli <aaronmorelli@zoho.com>
2016-09-29 15:24:05 -0700

    Create MemObjs.sql

---
+++ SQLOS/MemObjs.sql
@@ -0,0 +1,14 @@
+--Various links I was reviewing while researching entries in sys.dm_os_memory_objects that don't
+--have a matching Memory Clerk in sys.dm_os_memory_clerks
+
+http://support.microsoft.com/kb/970648
+http://support.microsoft.com/kb/924954
+
+http://blogs.msdn.com/b/sqlprogrammability/archive/2006/04/27/585170.aspx
+http://blogs.msdn.com/b/sqlprogrammability/archive/2007/01/09/1-0-structure-of-the-plan-cache-and-types-of-cached-objects.aspx
+http://blogs.msdn.com/b/sqlprogrammability/archive/2006/05/04/ketan-duvedi.aspx
+http://blogs.msdn.com/b/psssql/archive/2009/03/03/troubleshooting-sql-server-2005-2008-performance-counter-collection-problems.aspx
+
+http://madhuottapalam1.blogspot.com/2007/01/monitoring-memory-requirement.html
+http://social.msdn.microsoft.com/Forums/en-US/sqldatabaseengine/thread/9d8cca31-f9ef-4337-9dbe-6dfa06d41468
+http://media.wiley.com/product_data/excerpt/45/04700474/0470047445.pdf
